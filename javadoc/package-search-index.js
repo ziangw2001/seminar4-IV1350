026df04865b0a8dae62a741b73326a5d1842c7b2
ziangw2001 <132618412+ziangw2001@users.noreply.github.com>
2023-06-04 13:04:42 +0200

    Add files via upload

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"se.kth.iv1350.groceryStore.controller"},{"l":"se.kth.iv1350.groceryStore.integration"},{"l":"se.kth.iv1350.groceryStore.model"},{"l":"se.kth.iv1350.groceryStore.startup"},{"l":"se.kth.iv1350.groceryStore.view"}];updateSearchResults();
